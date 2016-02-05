98e69214bd10816052fe9964746846c458d2b11a
splucena <splucena2011@gmail.com>
2016-02-05 14:47:41 +0800

    Added some testing

--- projects/meadowlark/site/meadowlark.js
+++ projects/meadowlark/site/meadowlark.js
@@ -12,27 +12,21 @@ app.set('view engine','handlebars');
 
 app.set('port', process.env.PORT || 3030); 
 
-// custom 400 page
-/*app.use(function(req,res) {
-	res.type('text/plain');
-	res.status(404);
-	res.send('404 - Not Found');
-});*/
-
-// custom 500 page
-/*app.use(function(err,req,res,next) {
-	console.error(err.stack);
-	res.type('text/plain');
-	res.status(500);
-	res.send('500 - Server Error');
-});*/
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
 
 app.get('/',function(req,res){
 	res.render('home');
 });
 
 app.get('/about',function(req,res){
-	res.render('about', {fortune:fortune.getFortune()});
+	res.render('about', {
+		fortune:fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	});
+
 });
 
 app.use(function(req,res){
